078810a59c231590c9e545c2f5bf453992a50202
Stephen Justin <stevejustin1963@gmail.com>
2022-05-10 19:51:01 +1000

    Create ER_50percent.h

---
+++ ER_50percent.h
@@ -0,0 +1,11 @@
+: main 
+   1.6e-19 fstore e 
+   9e-31 fstore m 
+   3e8 fstore c 
+   c f@ 0.5 f* v f! 
+   v f@ v f@ f* m f@ f/ 2 f/ E f! 
+   E f@ e f@ f/ V f! 
+   "The voltage needed for an electron to travel at 50% the speed of light is " . 
+   V f@ . 
+   " volts." . 
+   cr ;
