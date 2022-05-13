1ad7341c28ddd6eec317bf3bd0385a8a5d2082ae
Stephen Justin <stevejustin1963@gmail.com>
2022-05-13 23:49:32 +1000

    Update findV_percentSpeed.c

--- findV_percentSpeed.c
+++ findV_percentSpeed.c
@@ -16,3 +16,16 @@ int main ()
   
   return 0;
 }
+
+matlab
+
+% find V for 50% speed of light.
+
+e = 1.6*10^(-19); %electronic charge
+m = 9*10^(-31); %mass of electron
+c = 3*10^8; %speed of light
+v = c*0.5; %speed
+E = m*v^2/2; %energy
+V = E/e; %voltage
+
+fprintf('The voltage needed for an electron to travel at 50%% the speed of light is %f volts.\n', V)
